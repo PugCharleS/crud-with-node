67891e05d8f3088500c06f1ae104c560d764b305
Carlos Solis <cmst010@gmail.com>
2022-07-15 04:30:56 -0500

    tabla creada con usuarios

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 require("dotenv").config({ path: "./config/vars/.env" });
 const bodyParser = require("body-parser");
 const { router } = require("./routes");
+const cors = require("cors");
 
 const PORT = process.env.PORT;
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
